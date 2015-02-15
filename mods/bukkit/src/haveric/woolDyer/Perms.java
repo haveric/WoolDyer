3759384d21f2248b5bf58cfb0a6038ddeb86eeee
Ryan Breuer <breuerryan@gmail.com>
2015-02-15 15:40:29 -0600

    Refactor project for multiple mod environments

---
+++ mods/bukkit/src/haveric/woolDyer/Perms.java
@@ -0,0 +1,15 @@
+package haveric.woolDyer;
+
+import org.bukkit.entity.Player;
+
+public class Perms {
+    private static final String permDye = "wooldyer.dye";
+
+    public static boolean canDye(Player player) {
+        return player.hasPermission(permDye);
+    }
+
+    public static String getPermDye() {
+        return permDye;
+    }
+}
